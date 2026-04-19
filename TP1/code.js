64e8a152825a0a1ff357fed32992ae9f94cea5a2
ikram02-el <ikramelmazini02@gmail.com>
2026-04-19 13:24:49 +0200

    Update code.js

--- TP1/code.js
+++ TP1/code.js
@@ -1,1 +1,18 @@
 
+// addition(a, b) -> retourne la somme de a et b
+function addition(a, b) {
+  const x = Number(a), y = Number(b);
+  if (Number.isNaN(x) || Number.isNaN(y)) return 0;
+  return x + y; // simple addition
+}
+
+function soustraction(a, b) {
+  return a - b;
+}
+
+if (require.main === module) {
+  console.log("Résultat addition:", addition(5, 3));
+  console.log("Résultat soustraction:", soustraction(5, 3));
+}
+
+module.exports = { addition, soustraction };
